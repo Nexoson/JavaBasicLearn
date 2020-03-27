69f9b7f2a2271a8764a2f28012e8dd62ef9c8bd2
Nexoson <1018458278@qq.com>
2020-03-27 20:15:22 +0800

    <volatile关键字>

---
+++ src/com/max/learn/thread/lesson03/OutOfOrderDemo07.java
@@ -0,0 +1,60 @@
+package com.max.learn.thread.lesson03;
+
+
+/**
+ * @ClassName OutOfOrderDemo07
+ * @Descripition 目标: 研究重排序情况可能带来的问题
+ * @Auther huangX
+ * @Date 2020/3/27 16:04
+ * @Version 1.0
+ **/
+public class OutOfOrderDemo07 {
+    // 新建几个静态变量
+    public static int a = 0, b = 0;
+    public static int i = 0, j = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        int count = 0;
+        while (true) {
+            count++;
+            a = 0;
+            b = 0;
+            i = 0;
+            j = 0;
+            // 定义两个线程
+            // 线程A
+            Thread t1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    a = 1;
+                    i = b;
+                }
+            });
+
+            // 线程B
+            Thread t2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    b = 1;
+                    j = a;
+                }
+            });
+
+            // 得到线程执行完毕以后变量的结果
+            t1.start();
+            t2.start();
+            // 让t1线程优先执行完毕
+            t1.join();
+            // 让t2线程优先执行完毕
+            t2.join();
+
+            // 得到线程执行完毕以后变量的结果
+            System.out.println("第" + count + "次 i =" + i + ", j=" + j);
+            if (i == 0 && j == 0) {
+                break;
+            }
+        }
+    }
+}
