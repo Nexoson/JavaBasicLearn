cc17c1abafad2b0df2c206005679c546426ccd5a
Nexoson <1018458278@qq.com>
2020-04-18 10:40:51 +0800

    <格式整理>

--- src/com/max/learn/集合相关/不安全集合/ContainerNotSafeDemo.java
+++ src/com/max/learn/集合相关/不安全集合/ContainerNotSafeDemo.java
@@ -19,64 +19,64 @@ public class ContainerNotSafeDemo {
 //        list.forEach(System.out::println);
         List<String> list = Collections.synchronizedList(new ArrayList<>());
 
-        for (int i=1; i<=30; i++){
-            new Thread(()->{
-                list.add(UUID.randomUUID().toString().substring(0,8));
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
                 System.out.println(list);
-            },String.valueOf(i)).start();
+            }, String.valueOf(i)).start();
         }
 
 
 
 
-        /**
-         * 1.故障现象
-         * java.util.ConcurrentModificationException 并发修改异常
-         *
-         * 2.导致原因
-         *   并发争抢修改导致,参考我们的花名册签名情况.
-         *   一个人正在写入,另一个同学过来抢夺,导致数据不一致异常,并发修改异常
-         * 3.解决方案
-         *   3.1 可以用Vector,但是性能差
-         *   3.2 用Collections.synchronizedList(new ArrayList<>());
-         * 4.优化建议(同样的操作不犯2次)
-         **/
+    /*
+      1.故障现象
+      java.util.ConcurrentModificationException 并发修改异常
+
+      2.导致原因
+        并发争抢修改导致,参考我们的花名册签名情况.
+        一个人正在写入,另一个同学过来抢夺,导致数据不一致异常,并发修改异常
+      3.解决方案
+        3.1 可以用Vector,但是性能差
+        3.2 用Collections.synchronizedList(new ArrayList<>());
+      4.优化建议(同样的操作不犯2次)
+     */
 
     }
 
-    
+
     /**
+     * @return List
      * @Author huangX
      * @Date 20:18 2020/4/17
-     * @Param 
-     * @return 
-     * @Description Vertor线程安全,但是性能太差
+     * @Param
+     * @Description Vertor线程安全, 但是性能太差
      **/
-    public List<String> getCurList1(){
+    public List<String> getCurList1() {
         return new Vector<>();
     }
 
-    
+
     /**
+     * @return List
      * @Author huangX
      * @Date 20:18 2020/4/17
-     * @Param 
-     * @return 
+     * @Param
      * @Description 线程安全
      **/
-    public List<String> getCurList2(){
+    public List<String> getCurList2() {
         return Collections.synchronizedList(new ArrayList<>());
     }
 
-    
+
     /**
+     * @return List
      * @Author huangX
      * @Date 20:18 2020/4/17
-     * @Param 
-     * @return 
+     * @Param
      * @Description 线程安全
      **/
-    public List<String> getCurList3(){
+    public List<String> getCurList3() {
         return new CopyOnWriteArrayList<>();
     }
 
