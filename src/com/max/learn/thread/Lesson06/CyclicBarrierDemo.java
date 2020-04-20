9111fb9d5fca585a406ef8de5a6712aef9d61d8f
Nexoson <1018458278@qq.com>
2020-04-20 11:25:44 +0800

    收集龙珠 CyclicBarrier (多个线程协同完成任务,满足一定数量后执行完成操作)

---
+++ src/com/max/learn/thread/Lesson06/CyclicBarrierDemo.java
@@ -0,0 +1,34 @@
+package com.max.learn.thread.Lesson06;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @ClassName CyclicBarrierDemo
+ * @Auther huangX
+ * @Date 2020/4/20 11:10
+ * @Version 1.0
+ * @Descripition 集齐龙珠
+ **/
+public class CyclicBarrierDemo {
+
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
+            System.out.println("*************召唤神龙");
+        });
+
+        for (int i = 1; i <= 7; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t 收集到第: "+tempInt +"龙珠");
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }, String.valueOf(i)).start();
+        }
+    }
+}
