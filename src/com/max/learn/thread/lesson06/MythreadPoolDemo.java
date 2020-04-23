7c285718cc9b18d01df81390f4c018ff1ef27892
Nexoson <1018458278@qq.com>
2020-04-23 20:39:12 +0800

    第四种获得/使用Java多线程的方式->线程池

---
+++ src/com/max/learn/thread/lesson06/MythreadPoolDemo.java
@@ -0,0 +1,34 @@
+package com.max.learn.thread.lesson06;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @ClassName MythreadPoolDemo
+ * @Auther huangX
+ * @Date 2020/4/23 20:28
+ * @Version 1.0
+ * @Descripition 第四种获得/使用Java多线程的方式,线程池
+ **/
+public class MythreadPoolDemo {
+
+    public static void main(String[] args) {
+
+        // 初始化5个固定线程的线程池
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+
+        // 模拟10个用户来办理业务,每个用户就是一个来自外部的请求线程
+        try {
+            for (int i = 1; i <= 10; i++) {
+                threadPool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+}
