cd3a3fc0819d22805e7619121966fd87910a7bb9
Max <1018458278@qq.com>
2020-04-24 17:10:14 +0800

    整理代码

--- src/com/max/learn/thread/lesson06/MythreadPoolDemo.java
+++ src/com/max/learn/thread/lesson06/MythreadPoolDemo.java
@@ -17,13 +17,13 @@ public class MythreadPoolDemo {
         // 初始化5个固定线程的线程池(执行长期的任务,性能好很多)
         // ExecutorService threadPool = Executors.newFixedThreadPool(5);
         // 只有一个线程的线程池(一个任务一个任务执行的场景)
-//         ExecutorService threadPool = Executors.newSingleThreadExecutor();
+        // ExecutorService threadPool = Executors.newSingleThreadExecutor();
         // N个线程的线程池(执行很多短期异步的小程序或者负载较轻的服务器)
         ExecutorService threadPool = Executors.newCachedThreadPool();
 
         // 模拟10个用户来办理业务,每个用户就是一个来自外部的请求线程
         try {
-            for (int i = 1; i <= 1000; i++) {
+            for (int i = 1; i <= 10; i++) {
                 threadPool.execute(() -> {
                     System.out.println(Thread.currentThread().getName() + "\t 办理业务");
                 });
