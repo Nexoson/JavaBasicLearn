3781feb73babfc0518e579357f60ff61d01c4cd3
Max <1018458278@qq.com>
2020-04-29 17:01:21 +0800

    优化代码

--- src/com/max/learn/其它/Idea_debug相关/TestDemo.java
+++ src/com/max/learn/其它/Idea_debug相关/TestDemo.java
@@ -3,6 +3,7 @@ package com.max.learn.其它.Idea_debug相关;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -15,39 +16,42 @@ import java.util.List;
 public class TestDemo {
     public static void main(String[] args) {
 //        int [] nums = {2,8,-1,0,12,0,-15,12,19,31,99,-7};
-        int [] nums = {0,4,3,0};
-
-//         Scanner input = new Scanner(System.in);
-//         System.out.println("请输入一个整数(1-37):");
-//         int target = input.nextInt();
+        int[] nums = {0, 4, 3, 0};
         int target = 0;
-//         int temp = Arrays.binarySearch(nums, target);
-        int[] ints = twoSum(nums, target);
+        int[] ints = twoSum01(nums, target);
         System.out.println(ints[0]);
         System.out.println(ints[1]);
-
-
     }
 
     private static int[] twoSum(int[] nums, int target) {
-        int [] ansewr = new int[2];
-        for (int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             // 判断输入数字与循环元素对比, 大于等于循环元素进入逻辑,否则循环继续
-                ansewr[0] = i;
-                for (int j = i+1; j<nums.length; j++){
-                    if (target-nums[i] == nums[j]){
-                        ansewr[1] = j;
-                        return ansewr;
-                    }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[]{i, j};
                 }
+            }
             continue;
         }
 
         return null;
     }
 
+    private static int[] twoSum01(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(16);
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.keySet().contains(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i],i);
+        }
+
+        return null;
+    }
+
     @Test
-    public void method01(){
+    public void method01() {
         // Creating a list of Integers
         List<Integer> list = Arrays.asList(3, 4, 6, 12, 20);
 
