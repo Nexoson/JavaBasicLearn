b0d074184a4b58165dc11f8491458c3ff3300e92
Nexoson <1018458278@qq.com>
2020-04-30 15:07:03 +0800

    建造者模式补充

--- src/com/max/learn/design_patterns/build_method/demo02/Click.java
+++ src/com/max/learn/design_patterns/build_method/demo02/Click.java
@@ -13,6 +13,9 @@ public class Click {
 //        showBike(new MobikeBuilder());
     }
     private static void showBike(Builder builder) {
+        // 指挥者类 构造方法引入 实际建造者(入参为子类构造者)
+        // construct()方法中也由实际建造者 生成自己所属的类(Ofo造自己的零件)
+
         Director director = new Director(builder);
         Bike bike = director.construct();
         bike.getFrame().frame();
