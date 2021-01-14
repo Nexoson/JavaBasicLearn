7a38ab5b7310ef150b3d72f320344109913b97de
Nexoson <1018458278@qq.com>
2021-01-14 14:26:53 +0800

    获取当前运行环境cpu的核心数

--- src/com/max/learn/集合相关/aaa.java
+++ src/com/max/learn/集合相关/aaa.java
@@ -2,10 +2,14 @@ package com.max.learn.集合相关;
 
 /**
  * @ClassName aaa
- * @Descripition TODO
+ * @Descripition 获取当前运行环境cpu的核心数
  * @Auther huangX
  * @Date 2019/8/18 20:53
  * @Version 1.0
  **/
 public class aaa {
+    public static void main(String[] args) {
+        System.out.printf("当前主机核心数: "+Runtime.getRuntime().availableProcessors());
+    }
+
 }
