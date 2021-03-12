8561f745969f9b7afd916baf5a33df80913147a9
huangxun2000 <huangxun888888@gmail.com>
2021-03-12 20:26:52 +0800

    Optional 学习

--- src/com/max/learn/Java8新特性/Optional/demo01/Test01.java
+++ src/com/max/learn/Java8新特性/Optional/demo01/Test01.java
@@ -14,10 +14,14 @@ public class Test01 {
 
     public static void main(String[] args) {
         // 对象判空, 返回其中一个字段
-        Student stu = new Student("张三", 18, "男");
-        System.out.println(getGender(null));
-        System.out.println(getGenderByOptional(null));
-
+        Student stu = new Student("周星星", 18, "男");
+//        System.out.println(getGender(null));
+//        System.out.println(getGenderByOptional(null));
+//        printName(stu);
+//        filter(stu);
+        Optional<Integer> age = getAge(stu);
+        Integer integer = age.get();
+        System.out.println(integer);
     }
 
     public static String getGender(Student student) {
@@ -30,4 +34,21 @@ public class Test01 {
     public static String getGenderByOptional(Student student) {
         return Optional.ofNullable(student).map(u -> u.getGender()).orElse("unKnow");
     }
+
+    /**ifPresent*/
+    public static void printName(Student student){
+        Optional.ofNullable(student).ifPresent(u ->  System.out.println("The student name is : " + u.getName()));
+    }
+
+    /**filter()*/
+    public static void filter(Student student){
+        Optional.ofNullable(student).filter(e -> e.getAge()> 19).ifPresent( u -> System.out.println("The student name is: "+ u.getName()));
+    }
+
+    /**map()*/
+    public static Optional<Integer> getAge(Student stu){
+        return Optional.ofNullable(stu).map(u -> u.getAge());
+    }
+
+
 }
